4df7da1ddb0efe1036e2402e40aaf837be9631ad
stanochka <krozzochka@gmail.com>
2021-06-04 13:54:25 +0300

    Make control buttons and add clear function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const makeGrid = () => {
   for (let i = 0; i < 16*16; i++) {
     container.style['grid-template-columns'] = 'repeat(16, 1fr)';
     let element = document.createElement('div');
-    element.style.border = '1px solid rgba(195, 195, 195)';
+    element.style.border = '1px solid rgba(195, 195, 195, 0.6)';
     element.id = `cell${i}`;
     container.appendChild(element);
   }
@@ -17,5 +17,20 @@ const divs = document.querySelectorAll('#container>div');
 divs.forEach((div) => {
   div.addEventListener('mouseover', () => {
     div.style.background = '#6f1087';
+  });
+});
+
+const clearCanvas = () => {
+  divs.forEach((div) => {
+    div.style.background = 'white';
+  })
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.id == 'clearButton') {
+      clearCanvas()
+    }
   });
 });
